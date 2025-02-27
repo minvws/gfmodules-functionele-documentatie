39c28f1ddbcba46c3e256cc49414ee613598a067
Randhir Gulraj <randh1r@hotmail.com>
2025-02-27 15:33:28 +0100

    feat: add rijksoverheid theme and config

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,23 @@ language = "nl"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "alabaster"
+# html_theme = "alabaster"
 html_static_path = ["_static"]
 html_show_sphinx = False
 html_show_sourcelink = False
+
+# -- Theme: Rijksoverheid UI and Options for HTML output -----------
+#
+# Download the theme and copy it to the source directory of the sphinx project:
+# https://github.com/minvws/icore-manon-sphinx-theme/actions/runs/13544738793/artifacts/2655913970
+
+html_theme = 'rijksoverheid_ui_theme_v0.1.0-beta.4'
+html_theme_path = ["."]
+locale_dirs = ['locales/']
+
+html_theme_options = {
+    "description": "Generieke Functies",
+    "github_user": "minvws",
+    "github_repo": "gfmodules-functional-documentation",
+    "tidelift_url": "https://github.com/minvws/gfmodules-functional-documentation",
+}
